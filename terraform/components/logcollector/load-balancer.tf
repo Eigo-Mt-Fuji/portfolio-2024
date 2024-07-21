8839a1e01a080c3fe6c021d61b03ffcdf1419e67
efg.river@gmail.com <efg.river@gmail.com>
2024-07-21 10:00:14 +0000

    lets fluentbit with nlb

---
+++ terraform/components/logcollector/load-balancer.tf
@@ -0,0 +1,28 @@
+resource "aws_lb" "nlb" {
+  name               = "fluentbit-nlb"
+  internal           = true
+  load_balancer_type = "network"
+
+  subnet_mapping {
+    subnet_id = var.nlb_subnet[0]
+  }
+}
+
+resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = 10224
+  protocol          = "TCP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.nlb_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "nlb_tg" {
+  name     = "fluentbit-tg"
+  port     = 10224
+  protocol = "TCP"
+  vpc_id   = var.vpc_id
+#  target_type = "ip"
+}
